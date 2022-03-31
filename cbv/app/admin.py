7a87e4f9e30377c73a31863853a166a1490f8564
Ravi Prakash Yadav <76773515+ravifact9648@users.noreply.github.com>
2022-03-31 12:15:04 +0530

    Delete cbv directory

--- cbv/app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from app.models import Beer
-
-class BeerAdmin(admin.ModelAdmin):
-    list_display=['name','taste','color','price']
-
-admin.site.register(Beer,BeerAdmin)
